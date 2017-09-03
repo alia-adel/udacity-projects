d158b623d537d09374552ec2a0721ca58fdfb0af
alia-adel <alia.adel.83@gmail.com>
2017-09-03 20:22:28 +0200

    feat: Add a reset button & apply code formatting

--- Pixel-Art-Maker/scripts/designs.js
+++ Pixel-Art-Maker/scripts/designs.js
@@ -1,55 +1,51 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
 /**
-* @description Draws the grid based on the user's entered height & width
-*/
+ * @description Draw the grid based on the user's entered height & width
+ */
 function makeGrid() {
-	// Your code goes here!
-	// Get the height & width values & draw the table.
-	const tableRowHtml =
-		`<tr>
+    const tableRowHtml =
+        `<tr>
 		</tr>`;
-	const tableColHtml = `<td></td>`;
+    const tableColHtml = `<td></td>`;
 
-	//Get user's entered grid height
-	let gridHeight = $('#input_height').val();
+    //Get user's entered grid height
+    let gridHeight = $('#input_height').val();
 
-	//Get user's entered grid height
-	let gridWidth = $('#input_width').val();
+    //Get user's entered grid width
+    let gridWidth = $('#input_width').val();
 
-	//Draw a table with ${gridHeight} rows & ${gridWidth} as columns
-	if(gridHeight > 1 && gridWidth > 1){
-		for(let row=0; row<gridHeight; row++) {
-			$('#pixel_canvas').append(tableRowHtml);
+    //Draw a table with ${gridHeight} rows & ${gridWidth} as columns
+    if (gridHeight > 1 && gridWidth > 1) {
+        for (let row = 0; row < gridHeight; row++) {
 
-			for(let col=0; col<gridWidth; col++) {
-				$('tr').last().append(tableColHtml);
-			}
-		}
-		// Add width & Height to cells
-		$('td').attr('style', 'width: 20px; height: 20px');
+            $('#pixel_canvas').append(tableRowHtml);
 
-		// Add click events on cells
-		$('td').click(function(event) {
-			$(this).css('background-color', function() {
-				return $('#colorPicker').val();
-			});
-		});
+            for (let col = 0; col < gridWidth; col++) {
+                $('tr').last().append(tableColHtml);
+            }
 
-	} else {
-		alert('Entered Height & Width are not sufficient to draw the grid');
-	}
+        }
+
+        // Add click events on cells
+        $('td').click(function(event) {
+            $(this).css('background-color', function() {
+                return $('#colorPicker').val();
+            });
+        });
+
+    } else {
+        alert('Entered height & width are not sufficient to draw the grid');
+    }
 
 }
 
 // call makeGrid() when form gets submitted
-$('#sizePicker').submit(function(event){
-	event.preventDefault();
-	makeGrid();
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+    makeGrid();
 });
 
-
+//Resets sizePicker form & grid back to 1x1
+$('#resetBtn').click(function() {
+    $('tr').remove();
+    $('#colorPicker').val('#000');
+});
