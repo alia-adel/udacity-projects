f85fbbdf690123aaf72cefed75c0577dad82d786
alia-adel <alia.adel.83@gmail.com>
2017-09-22 13:56:45 +0200

    refactor: JavaScript file cleanup

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -1,25 +1,17 @@
-/*
- * Create a list that holds all of your cards
- */
+/**  Matching Game app **/
 
+
+/// Variables' Section ///
+
+// Constant to hold all pairs of cards icons' names, i.e.: fontawesome css classes
 const CARDS_ICONS = ['fa-camera', 'fa-futbol-o', 'fa-anchor', 'fa-bug', 'fa-bicycle', 'fa-diamond', 'fa-twitter', 'fa-car',
 'fa-camera', 'fa-futbol-o', 'fa-anchor', 'fa-bug', 'fa-bicycle', 'fa-diamond', 'fa-twitter', 'fa-car'];
 
+// Game card base css classes, used to reset the game card icon
 const CARD_ICON_BASE_CLASSES = 'fa fa-lg game-icons';
 
-let moveCounter = 0;
-
-let startRating = 0;
-
-let startDate = new Date();
-
-// Create cards objects' array
-let cards = [];
-
-// Retrieve all games boards cards html elements
-let cardsHtmlElem = document.getElementsByClassName('game-icons');
-
-let cssClasses = {
+// Object to hold the css classes used accross the app
+const cssClasses = {
 	flip : 'flip',
 	show : 'show',
 	correct: 'correct',
@@ -28,9 +20,31 @@ let cssClasses = {
 	dim : 'dim'
 };
 
+// Counter to count every click the user does.
+let moveCounter = 0;
+
+// Tracking time by logging the gamer start time
+let startDate = new Date();
+
+// Create game cards objects' array
+let cards = [];
+
+// Retrieve all games boards cards html elements
+let cardsHtmlElem = document.getElementsByClassName('game-icons');
+
+// Retrieve all stars elements
+let gameStars = document.getElementsByClassName('fa-star');
+
+// Global variable to hold the last flipped card which will be used to match to
 let lastFlippedCard;
 
-/* Card Class Definition */
+///	End of Variables' Section ///
+
+/**
+ * @description Represents a Game Card
+ * @constructor
+ * @param {string} cardIcon - The icon present on the game card
+ */
 let Card = function(cardIcon) {
 	let obj = Object.create(Card.prototype);
 	obj.icon = cardIcon;
@@ -54,32 +68,33 @@ Card.prototype.markCardCorrect = function() {
 	this.correct = true;
 	this.htmlElement = updateElementClasses(this.htmlElement, cssClasses.correct, true);
 };
-
-
 /* end of Card Class Definition */
 
 
+///	Functions' Section ///
 
-/*
- * 1- Shuffle icons
- * 2- create array of cards & set each object with an icon
- * 3- get all HTML Cards' Objects
- * 4- Attach evenlistener on Click & attach Html object on the array
- * 5- method on the class to show/hide card based on the html object
+/**
+ * @description Initialize the game board by doing the following steps:
+ * 				1- Shuffle the game cards' icons using 'shuffle' function
+ *				2- Create a card array filled with game card's object
+ *					- Fill each Card object with an icon
+ *					- Attach the equivelant HtmlElement object
+ *					- Attach onclick event on each card HtmlElement
+ *						1- Increment gamer move count
+ *						2- Update card flipping status, i.e. show or hide card
+ *						3- In case there was an alreday flipped card then check if they match
  */
-
-function startCardBoard() {
+function startGameBoard() {
+	let card;
 	// Shuffle the game board icons
 	let cardsIconsArr = shuffle(CARDS_ICONS);
 
-	let card;
-
-	// Create a card array
 	if(cardsHtmlElem != null && cardsIconsArr != null
 		&& (cardsHtmlElem.length === cardsIconsArr.length)) {
 
+		// Load the card array
 		for(let i = 0; i< cardsIconsArr.length; i++) {
-			// Reset any added classes
+			// Reset any added classes & add the updated one
 			cardsHtmlElem[i].className = `${CARD_ICON_BASE_CLASSES} ${cardsIconsArr[i]}`;
 			// Create new card object
 			card = new Card(cardsIconsArr[i]);
@@ -87,145 +102,44 @@ function startCardBoard() {
 			card.index = i;
 			cards.push(card);
 
-			// Add card event listener
+			// Add onclick event listener
 			card.htmlElement.addEventListener('click', function() {
+				/*
+				 * If the card is already flipped then don't count a new move
+				 * else increment the move counter & update the move value above the game deck
+				 */
 				if(!this.className.includes(cssClasses.show)){
 					moveCounter++;
-					document.getElementById('game-moves').innerHTML = moveCounter + ((moveCounter <= 1)?' Move':' Moves');
+					document.getElementById('game-moves').innerHTML
+						= moveCounter + ((moveCounter <= 1)?' Move':' Moves');
 					calcualteMoveScore();
 				}
 
-				// Update card status & check matches
+				// Update card flipping status & check if it matches the already flipped one
 				if(!cards[i].correct) {
 					if(!cards[i].flipped) {
 						cards[i].showCard();
 					}
+					// This hack was added to force execution order
 					setTimeout(checkMatchingCards, 300, cards[i]);
 				}
 			});
-
 		}
 
 	} else {
-		console.log(`Error: Icons' array empty`);
-	}
-};
-
-/*
- * Add/Remove classes from an element
- */
-function updateElementClasses(element, className, add) {
-	if(add && !element.className.includes(className)) {
-		element.className = `${element.className} ${className}`;
-	} else if(!add && element.className.includes(className)) {
-		element.className = element.className.replace(className, '');
-	}
-	return element.className;
-};
-
-
-/*
- * Check if the last clicked card matches the already opened one
- */
-function checkMatchingCards(card) {
-	let cardIcon = '';
-	if(card.htmlElement.childNodes !=null && card.htmlElement.childNodes.length > 0
-		&& card.htmlElement.childNodes[1].className != null) {
-		cardIcon = card.htmlElement.childNodes[1].className.replace(CARD_ICON_BASE_CLASSES, '').trim();
-	}
-
-
-	if(lastFlippedCard != null) {
-		if(lastFlippedCard.flipped && !lastFlippedCard.correct
-			&& card.index != lastFlippedCard.index && card.flipped && (card.icon === lastFlippedCard.icon)) {
-			card.markCardCorrect();
-			lastFlippedCard.markCardCorrect();
-			lastFlippedCard = null;
-			checkGameCompletetion();
-
-		} else {
-			updateElementClasses(card.htmlElement, cssClasses.wrong, true);
-			updateElementClasses(lastFlippedCard.htmlElement, cssClasses.wrong, true);
-
-			window.setTimeout(function() {
-				alertWrongCards(card, lastFlippedCard);
-			}, 500);
-		}
-	} else {
-		lastFlippedCard = card;
-	}
-
-};
-
-function alertWrongCards(card, lastCard){
-
-	card.hideCard();
-	lastCard.hideCard();
-
-	updateElementClasses(card.htmlElement, cssClasses.wrong, false);
-	updateElementClasses(lastCard.htmlElement, cssClasses.wrong, false);
-
-	lastFlippedCard = null;
-}
-
-function calcualteMoveScore() {
-	console.log(`Move counts ${moveCounter}`);
-	if(moveCounter > CARDS_ICONS.length) {
-		updateElementClasses(document.getElementById('star1'), cssClasses.dim, true);
-	}
-	if(moveCounter > (CARDS_ICONS.length * 2)) {
-		updateElementClasses(document.getElementById('star2'), cssClasses.dim, true);
-	}
-	if(moveCounter > (CARDS_ICONS.length * 3)) {
-		updateElementClasses(document.getElementById('star3'), cssClasses.dim, true);
+		console.log(`Error: Game html cards element or/& Icons' array is empty`);
 	}
 }
 
-function calculateGameTime() {
-	return (new Date() - startDate)/1000;
-}
 
-
-
-function checkGameCompletetion() {
-	for(card of cards) {
-		if(!card.correct) {
-			return false;
-		}
-	}
-	let succesMsg = document.getElementsByClassName('success-sub-msg')[0];
-	console.log(succesMsg.innerHTML);
-	succesMsg.innerHTML = succesMsg.innerHTML.replace('#{move}', moveCounter).replace(
-		'#{time}', calculateGameTime()).replace(
-		'#{stars}', (3-document.getElementsByClassName('dim').length));
-
-	updateElementClasses(document.getElementById('game-modal'), cssClasses.fullScreen, true);
-	updateElementClasses(document.getElementById('game-success-alert'), cssClasses.fullScreen, true);
-
-	document.getElementById('game-modal').addEventListener('click', function(){
-		document.getElementById('game-modal').className
-			= (document.getElementById('game-modal').className).replace(cssClasses.fullScreen, '');
-		document.getElementById('game-success-alert').className
-			= (document.getElementById('game-success-alert').className).replace(cssClasses.fullScreen, '');
-	});
-
-	return true;
-}
-
-
-
-startCardBoard();
-
-
-
-/*
- * Display the cards on the page
+/**
+ * @description Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ *	 * Shuffle function from http://stackoverflow.com/a/2450976
+ * @param {object} array - The array with items to shuffle
  */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -241,16 +155,162 @@ function shuffle(array) {
 }
 
 
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+/**
+ * @description Track gamer card clicks,
+ *       update the move count above the game deck
+ *       & dims the game deck's stars accoording to the following conditions:
+ *         - Dim one star if clicks are greater than 16 moves.
+ *         - Dim two star if clicks are greater than 32 moves.
+ *         - Dim three star if clicks are greater than 48 moves.
+ *
  */
+function calcualteMoveScore() {
+	if(gameStars != null && gameStars.length == 3) {
+		if(moveCounter > (cards.length * 3)) {
+			updateElementClasses(gameStars[2], cssClasses.dim, true);
+		} else if(moveCounter > (cards.length * 2)) {
+			updateElementClasses(gameStars[1], cssClasses.dim, true);
+		} else if(moveCounter > cards.length) {
+			updateElementClasses(gameStars[0], cssClasses.dim, true);
+		}
+	}
+}
+
+
+/**
+ * @description Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ *	 * Shuffle function from http://stackoverflow.com/a/2450976
+ * @param {object} element - The HTML dom element that we need to update its css classes' value
+ * @param {string} className - The css class name that needs to be added or removed
+ * @param {boolean} add - In case a css class name needs to be added
+ *                        then this value will be 'true' else use 'false' to remove it if it exists
+ * @returns {string} HTML dom element updated css classes' string
+ */
+function updateElementClasses(element, className, add) {
+	if(add && !element.className.includes(className)) {
+		element.className = `${element.className} ${className}`;
+	} else if(!add && element.className.includes(className)) {
+		element.className = element.className.replace(className, '');
+	}
+	return element.className;
+}
+
+
+/**
+ * @description Check if the given card matches the already flipped card
+ * @param {object} card - An item from the 'Card' class which we need to check if it has a match
+ */
+function checkMatchingCards(card) {
+	/*
+	 * 1- Check if there already exists another flipped card
+	 * 2- Check if the index of the last flipped card is not the same as the one we are trying to match
+	 * 3- Check that the Game card icon is the same for the current card & the already flipped card
+	 * 4- Mark both matched card as correct & change their css style class
+	 * 5- Check if with this move the game has completed or not by calling function 'checkGameCompletetion'
+	 * 6- In case the cards don't match then alert the user by shaking the cards & hide them both
+	 */
+	if(lastFlippedCard != null) {
+		if(lastFlippedCard.flipped && !lastFlippedCard.correct
+			&& card.index != lastFlippedCard.index && card.flipped && (card.icon === lastFlippedCard.icon)) {
+			card.markCardCorrect();
+			lastFlippedCard.markCardCorrect();
+			lastFlippedCard = null;
+			checkGameCompletetion();
+
+		} else {
+			updateElementClasses(card.htmlElement, cssClasses.wrong, true);
+			updateElementClasses(lastFlippedCard.htmlElement, cssClasses.wrong, true);
+			// This hack was added to force execution order
+			setTimeout(function() {
+				alertWrongCards(card, lastFlippedCard);
+			}, 500);
+		}
+	} else {
+		lastFlippedCard = card;
+	}
+
+}
+
+
+/**
+ * @description Incase cards dont match then shake the cards & hide them
+ * @param {object} card - The recently clicked on game card
+ * @param {object} lastCard - The last clicked on game card
+ */
+function alertWrongCards(card, lastCard){
+
+	card.hideCard();
+	lastCard.hideCard();
+
+	updateElementClasses(card.htmlElement, cssClasses.wrong, false);
+	updateElementClasses(lastCard.htmlElement, cssClasses.wrong, false);
+
+	lastFlippedCard = null;
+}
+
+
+/**
+ * @description Check if all cards are flipped & matched to display the Completion message
+ * @returns {boolean} true in case all cards were matched & false otherwise
+ */
+function checkGameCompletetion() {
+	// Check if all game cards were matched
+	for(let card of cards) {
+		if(!card.correct) {
+			return false;
+		}
+	}
+
+	// Update the success message by replacing placeholders for moves, time & stars
+	let succesMsg = document.getElementsByClassName('success-sub-msg')[0];
+	succesMsg.innerHTML = succesMsg.innerHTML.replace('#{move}', moveCounter).replace(
+		'#{time}', calculateGameTime());
+
+
+	let dimmedStars = document.getElementsByClassName('dim').length;
+	if(dimmedStars > 0) {
+		succesMsg.innerHTML
+			= succesMsg.innerHTML.replace(
+				'#{stars}', `& with ${3-dimmedStars} ${((3-dimmedStars) > 1)?'stars' : 'star' }`);
+	} else {
+		succesMsg.innerHTML = succesMsg.innerHTML.replace('#{stars}','');
+	}
+
+	// Maximize the success modal windows
+	updateElementClasses(document.getElementById('game-modal'), cssClasses.fullScreen, true);
+	updateElementClasses(document.getElementById('game-success-alert'), cssClasses.fullScreen, true);
+
+	// Add an onclick event to hide the success modal windows when the background is clicked
+	document.getElementById('game-modal').addEventListener('click', function(){
+		document.getElementById('game-modal').className
+			= (document.getElementById('game-modal').className).replace(cssClasses.fullScreen, '');
+		document.getElementById('game-success-alert').className
+			= (document.getElementById('game-success-alert').className).replace(cssClasses.fullScreen, '');
+	});
+
+	return true;
+}
+
+
+/**
+ * @description Calculates the time in seconds between the game start & the game completion
+ * @returns {number} time in seconds
+ */
+function calculateGameTime() {
+	return (new Date() - startDate)/1000;
+}
+
+
+///	End of Functions' Section ///
+
+
+
+/* Start the game board */
+document.addEventListener('DOMContentLoaded',function(){
+	startGameBoard();
+});
+
 
